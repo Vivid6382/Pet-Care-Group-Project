f27f1b55481b5596479885c646eab0a2f994549a
Vivid <vietnc.23bi14449@usth.edu.vn>
2025-12-07 15:34:57 +0700

    Merge everyone work into main branch

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -10,23 +10,23 @@ const NavBar = () => {
     <div className="sticky top-0 z-40 transition-none box-border">
         <ul className="list-none bg-white flex w-full p-4 justify-around gap-5 items-center">
             <li><Link className="decoration-0 font-extrabold text-blue-500 text-5xl cursor-pointer" href="#">PetCare 🐾</Link></li>
-            <li><Link className="commonbtn" href="calculator">Pet feeding <br/>calculator 🍖</Link></li>            
-            <li><Link className="commonbtn" href="calendar">Pet events <br/>calendar 📅</Link></li>            
-            <li><Link className="commonbtn" href="tracker">Tracking pet <br/>health 📊</Link></li>
+            <li><Link className="commonbtn" href="/calculator">Pet feeding <br/>calculator 🍖</Link></li>            
+            <li><Link className="commonbtn" href="/calendar">Pet events <br/>calendar 📅</Link></li>            
+            <li><Link className="commonbtn" href="/tracker">Tracking pet <br/>health 📊</Link></li>
             <li className="group">
               <Link className="commonbtn" href='#'>News ▼
               </Link>
               <ul className="bg-white absolute z-50 hidden group-hover:block">
-                <li><Link className={SubMenu} href="news/admin">Admin</Link></li>
-                <li><Link className={SubMenu} href="news/user">User</Link></li>
-                <li><Link className={SubMenu} href="news/favorite">Favorite</Link></li>
+                <li><Link className={SubMenu} href="/news/admin">Admin</Link></li>
+                <li><Link className={SubMenu} href="/news/user">User</Link></li>
+                <li><Link className={SubMenu} href="/news/favorite">Favorite</Link></li>
               </ul>
               
             </li>
             <li>
               <ul className="flex gap-5 justify-center">
-                  <li><a href="login" className="block decoration-0 px-10 py-3 text-center items-center text-black bg-white border-4 border-black border-solid rounded-full cursor-pointer text-1xl font-bold box-border transition-all duration-200 ease-in-out hover:bg-black hover:text-white">Log in</a></li>
-                  <li><a href="signin" className="block decoration-0 px-10 py-3 text-center items-center text-black bg-white border-4 border-black border-solid rounded-full cursor-pointer text-1xl font-bold box-border transition-all duration-200 ease-in-out hover:bg-black hover:text-white">Sign in</a></li>
+                  <li><a href="/login" className="block decoration-0 px-10 py-3 text-center items-center text-black bg-white border-4 border-black border-solid rounded-full cursor-pointer text-1xl font-bold box-border transition-all duration-200 ease-in-out hover:bg-black hover:text-white">Log in</a></li>
+                  <li><a href="/signin" className="block decoration-0 px-10 py-3 text-center items-center text-black bg-white border-4 border-black border-solid rounded-full cursor-pointer text-1xl font-bold box-border transition-all duration-200 ease-in-out hover:bg-black hover:text-white">Sign in</a></li>
               </ul>
             </li>
         </ul>
